e17136df48814cecc715404254be5b3b1f0e8016
alexgrach <gav@bmstu.ru>
2023-03-11 13:36:31 +0300

    Update Configuration.h

--- kp3s/Configuration.h
+++ kp3s/Configuration.h
@@ -1270,9 +1270,9 @@
  */
 //#define CLASSIC_JERK
 #if ENABLED(CLASSIC_JERK)
-  #define DEFAULT_XJERK 10.0
-  #define DEFAULT_YJERK 10.0
-  #define DEFAULT_ZJERK  0.3
+  #define DEFAULT_XJERK 15.0
+  #define DEFAULT_YJERK 15.0
+  #define DEFAULT_ZJERK  0.4
   //#define DEFAULT_IJERK  0.3
   //#define DEFAULT_JJERK  0.3
   //#define DEFAULT_KJERK  0.3
